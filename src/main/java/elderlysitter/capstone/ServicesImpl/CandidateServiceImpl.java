c24f45bde07a4cdd756a153ae3a4af85766b3184
ttson1001 <ttson1001@gmail.com>
2022-10-27 17:19:12 +0700

    update accept api

--- src/main/java/elderlysitter/capstone/ServicesImpl/CandidateServiceImpl.java
+++ src/main/java/elderlysitter/capstone/ServicesImpl/CandidateServiceImpl.java
@@ -74,7 +74,7 @@ public class CandidateServiceImpl implements CandidateService {
                 list.add(dto);
             }
         }catch (Exception e){
-
+            e.printStackTrace();
         }
         return list;
     }
